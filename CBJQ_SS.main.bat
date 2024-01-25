6e4f9d6f6c17d6326f15955ffcc3348e3b70d935
刘杰文 <1635370493@qq.com>
2024-01-25 11:22:48 +0800

    edition v1

--- CBJQ_SS.main.bat
+++ CBJQ_SS.main.bat
@@ -1,28 +1,47 @@
 @echo off
 
+@REM ----------------------------------------------
+@REM 运行环境注意
+
+@REM 1. 从Powershell启动可能会存在LANG环境变量
+
+@REM ----------------------------------------------
+@REM 程序初始化阶段1
+
 @REM Set codepage to UTF-8(65001)
 @for /F "tokens=2 delims=:" %%i in ('chcp') do @( set /A codepage=%%i ) 
 @call :func_ensureACP
 
 @REM ----------------------------------------------
+@REM 用户变量预设值区
+
+@set launcher_none=cmd.exe /C Exit /B 1
+
+@REM ----------------------------------------------
 @REM 用户变量设定区
 
-@REM 以下三行请填入启动器路径(不加引号)。
-@set launcher_worldwide=
-@set launcher_bilibili=
-@set launcher_kingsoft=
+@REM 以下三行请填入启动器路径(不加引号，不可以为空)。
+@set launcher_worldwide=%launcher_none%
+@set launcher_bilibili=%launcher_none%
+@set launcher_kingsoft=%launcher_none%
 
-if not defined LANG (
+@REM 以下两句最多启用一个。
+@set LANG_default=zh
+@REM @set LANG_default=en
+
+@REM ----------------------------------------------
+@REM 程序初始化阶段2
+
+if not defined mLANG (
     @REM 设置默认语言
-    set LANG=zh
-    @REM set LANG=en
+    set mLANG=%LANG_default%
 )
 set LANG
 
 @REM ----------------------------------------------
 @REM 初始化已完成，正式进入程序。
 
-if /I "%LANG%" EQU "zh" (
+if /I "%mLANG%" EQU "zh" (
     call :func_programinfo_zh_cn
 ) else (
     call :func_programinfo_en_us
@@ -32,13 +51,17 @@ if /I "%LANG%" EQU "zh" (
 @REM 程序加载完成，开始工作。
 
 if /I "%~1" == "worldwide" (
-    if /I "%LANG%" == "zh" ( echo 启动国际服 ) else ( echo Start: worldwide )
-
+    if /I "%mLANG%" == "zh" ( echo 启动国际服 ) else ( echo Start: worldwide )
+    call "%launcher_worldwide%"
+    if ERRORLEVEL 1 (
+        echo 出错力！
+    )
 ) else if /I "%~1" == "bilibili" (
-    if /I "%LANG%" == "zh" ( echo 启动B服 ) else ( echo Start: bilibili )
-
+    if /I "%mLANG%" == "zh" ( echo 启动B服 ) else ( echo Start: bilibili )
+    start "%launcher_bilibili%"
 ) else if /I "%~1" == "kingsoft" (
-    if /I "%LANG%" == "zh" ( echo 启动官服 ) else ( echo Start: kingsoft )
+    if /I "%mLANG%" == "zh" ( echo 启动官服 ) else ( echo Start: kingsoft )
+    start "%launcher_kingsoft%"
 )
 
 @REM ----------------------------------------------
