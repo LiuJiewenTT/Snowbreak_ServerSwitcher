ee49972ae61774f98696eca5ea3db3cfd56183f8
LiuJiewenTT <1635370493@qq.com>
2025-08-09 12:15:24 +0800

    Update readme.js

--- readme.js
+++ readme.js
@@ -8,6 +8,7 @@ function add_script(src) {
 add_script('https://code.jquery.com/jquery-3.6.0.min.js');
 add_script('docs/doc_res/js/go_to_top.js');
 
-
-old_version_link = document.getElementById("old-version-link");
-old_version_link.setAttribute("href", 'old/');
+document.addEventListener("DOMContentLoaded", function() {
+    old_version_link = document.getElementById("old-version-link");
+    old_version_link.setAttribute("href", 'old/');
+});
